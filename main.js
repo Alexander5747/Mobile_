2d3fee0f87734e119f1158222e6ff5cd94a4b788
Alexander5747 <96681086+Alexander5747@users.noreply.github.com>
2023-01-02 19:45:04 +0300

    v6

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let burger_menu = document.querySelector('.burger_menu'),
 Mobile_menu__level1=document.querySelector('.Mobile_menu__level1'),
+Mobile_menu__level2=document.querySelector('.Mobile_menu__level2'),
 bm = document.querySelector('#bm'),
 b=document.querySelector('#b'),
 pod_of =document.querySelector('.pod_of'),
@@ -14,4 +15,10 @@ bm.addEventListener('click', function(){
 });
 b.addEventListener('click', function(){
     pod_of.classList.toggle('pod_on')
+});
+mb2.addEventListener('click', function(){
+    Mobile_menu__level2.classList.add('active2')
+});
+bm2.addEventListener('click', function(){
+    Mobile_menu__level2.classList.remove('active2')
 });
